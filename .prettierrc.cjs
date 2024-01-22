c4ef61695337e3c541f0ebfaff4798d0331230c7
zguiyang <zhaoguiyang18@gmail.com>
2024-01-22 21:02:01 +0800

    chore: add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+const config = {
+	trailingComma: "es5",
+	semi: true,
+	singleQuote: true,
+	jsxBracketSameLine: false,
+	arrowParens: "avoid",
+	printWidth: 80,
+	tabWidth: 2,
+	jsxSingleQuote: true,
+	bracketSpacing: true,
+	bracketSameLine: true,
+	importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	plugins: ["@trivago/prettier-plugin-sort-imports"],
+};
+
+module.exports = config;
--- packages/naive-ui-pro/layout/src/Layout.tsx
+++ packages/naive-ui-pro/layout/src/Layout.tsx
@@ -2,11 +2,8 @@ import { defineComponent } from 'vue';
 
 const Layout = defineComponent({
   name: 'pro-layout',
-  setup () {
-    return () => (
-        <div>Layout</div>
-    );
-  }
+  setup() {
+    return () => <div className='pro-layout'>Layout</div>;
+  },
 });
-
 export default Layout;
