c87cfda82309975995363fbc958f0e65e034c95c
zguiyang <zhaoguiyang18@gmail.com>
2024-02-06 16:14:47 +0800

    test: :white_check_mark: supported jsx component

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,11 @@
+import vue from '@vitejs/plugin-vue';
+import jsx from '@vitejs/plugin-vue-jsx';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  plugins: [vue(), jsx()],
   test: {
     include: ['./src/**/tests/*.{test,spec}.?(c|m)[jt]s?(x)'],
-    // ... Specify options here.
+    environment: 'happy-dom',
   },
 });
