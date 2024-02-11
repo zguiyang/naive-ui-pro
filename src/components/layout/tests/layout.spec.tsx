97d27d5db48c1eee3e776e6559d881598bf71128
zguiyang <zhaoguiyang18@gmail.com>
2024-02-11 10:59:30 +0800

    test: :white_check_mark: update test case of layout

--- src/components/layout/tests/layout.spec.tsx
+++ src/components/layout/tests/layout.spec.tsx
@@ -1,7 +1,54 @@
+import { mount } from '@vue/test-utils';
 import { describe, expect, it } from 'vitest';
 
-describe('side layout', () => {
-  it('test', () => {
-    expect(1).toBe(1);
+import { ProLayout } from '../index';
+
+describe('pro-layout', () => {
+  it('should render', () => {
+    mount(ProLayout);
+  });
+
+  it('should work with default', async () => {
+    const wrapper = mount(ProLayout, {
+      props: {},
+      slots: {
+        default: () => ['page container'],
+      },
+    });
+    expect(wrapper.find('.pro-layout').exists()).toBe(true);
+    expect(wrapper.find('.pro-layout-header').exists()).toBe(true);
+    expect(wrapper.find('.pro-layout-sidebar').exists()).toBe(true);
+    expect(wrapper.find('.pro-layout__footer').exists()).toBe(true);
+    expect(wrapper.find('.pro-layout__content').text()).toBe('page container');
+  });
+
+  it('should work with header height', async () => {
+    const wrapper = mount(ProLayout, {
+      props: {
+        headerHeight: 70,
+      },
+      slots: {},
+    });
+
+    const headerStyle = wrapper.find('.pro-layout-header').attributes('style');
+    const contentStyle = wrapper
+      .find('.pro-layout__container-wrapper')
+      .attributes('style');
+
+    expect(headerStyle).toContain('height: 70px;');
+    expect(contentStyle).toContain('top: 70px;');
+  });
+
+  it('should work with sidebar width', async () => {
+    const wrapper = mount(ProLayout, {
+      props: {
+        sidebarWidth: 200,
+      },
+      slots: {},
+    });
+    const sidebarStyle = wrapper
+      .find('.pro-layout-sidebar')
+      .attributes('style');
+    expect(sidebarStyle).toContain('width: 200px;');
   });
 });
