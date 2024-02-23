ce88294bb52932433953fe5cdf1d86b8fd480d15
zguiyang <zhaoguiyang18@gmail.com>
2024-02-23 14:15:55 +0800

    upd: :arrow_up: update code for dev-server

--- dev-server/src/App.vue
+++ dev-server/src/App.vue
@@ -25,117 +25,6 @@ const menuOptions = [
     key: 'pinball-1973',
     disabled: true,
     icon: renderIcon(BookIcon),
-    children: [
-      {
-        label: '鼠',
-        key: 'rat',
-      },
-    ],
-  },
-  {
-    label: '寻羊冒险记',
-    key: 'a-wild-sheep-chase',
-    disabled: true,
-    icon: renderIcon(BookIcon),
-  },
-  {
-    label: '舞，舞，舞',
-    key: 'dance-dance-dance',
-    icon: renderIcon(BookIcon),
-    children: [
-      {
-        type: 'group',
-        label: '人物',
-        key: 'people',
-        children: [
-          {
-            label: '叙事者',
-            key: 'narrator',
-          },
-          {
-            label: '羊男',
-            key: 'sheep-man',
-          },
-        ],
-      },
-      {
-        label: '饮品',
-        key: 'beverage',
-        children: [
-          {
-            label: '威士忌',
-            key: 'whisky',
-          },
-        ],
-      },
-      {
-        label: '食物',
-        key: 'food',
-        children: [
-          {
-            label: '三明治',
-            key: 'sandwich',
-          },
-        ],
-      },
-      {
-        label: '过去增多，未来减少',
-        key: 'the-past-increases-the-future-recedes',
-        icon: renderIcon(BookIcon),
-      },
-    ],
-  },
-  {
-    label: '且听风吟',
-    key: 'hear-the-wind-sing0',
-    icon: renderIcon(BookIcon),
-  },
-  {
-    label: '1973年的弹珠玩具',
-    key: 'pinball-1973',
-    disabled: true,
-    icon: renderIcon(BookIcon),
-    children: [
-      {
-        label: '鼠',
-        key: 'rat',
-      },
-    ],
-  },
-
-  {
-    label: '且听风吟',
-    key: 'hear-the-wind-sing3',
-    icon: renderIcon(BookIcon),
-  },
-  {
-    label: '1973年的弹珠玩具',
-    key: 'pinball-1974',
-    disabled: true,
-    icon: renderIcon(BookIcon),
-    children: [
-      {
-        label: '鼠',
-        key: 'rat',
-      },
-    ],
-  },
-  {
-    label: '且听风吟',
-    key: 'hear-the-wind-sing5',
-    icon: renderIcon(BookIcon),
-  },
-  {
-    label: '1973年的弹珠玩具',
-    key: 'pinball-1976',
-    disabled: true,
-    icon: renderIcon(BookIcon),
-    children: [
-      {
-        label: '鼠',
-        key: 'rat',
-      },
-    ],
   },
 ];
 function renderIcon(icon: Component) {
@@ -186,11 +75,13 @@ function handleUpdateCollapsed(collapsed: boolean) {
     v-model:collapsed="collapsedRef"
     :layout-mode="currentLayoutMode"
     content-width="fluid"
-    :side-props="{}"
     side-bar-width="200"
     copy-right-text="© 2024 Naive UI pro"
     :menu-props="{
       options: menuOptions,
+    }"
+    :footer-props="{
+      bordered: true,
     }"
     @update:collapsed="handleUpdateCollapsed">
     <!-- <template #left-side>LOGO区域</template> -->
